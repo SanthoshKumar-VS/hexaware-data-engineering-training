b6e2b82a8de5360528c5f21e9ec480e52d428861
SANTHOSH KUMAR V S <151427148+SanthoshKumar-VS@users.noreply.github.com>
2026-03-26 11:31:15 +0530

    Add files via upload

--- 26-03-2026.sql
+++ 26-03-2026.sql
@@ -1,141 +1,110 @@
-create database company_training;
-use company_training;
+create database capstone_sql;
+use capstone_sql;
 
-create table employees (
-    emp_id int primary key,
-    emp_name varchar(100),
-    department varchar(50),
-    city varchar(50)
+create table students (
+    student_id int primary key,
+    student_name varchar(100),
+    city varchar(50),
+    age int
 );
 
-create table projects (
-    project_id int primary key,
-    emp_id int,
-    project_name varchar(100),
-    project_budget decimal(12,2),
-    project_status varchar(50)
+create table enrollments (
+    enrollment_id int primary key,
+    student_id int,
+    course_name varchar(100),
+    trainer varchar(100),
+    fee decimal(10,2)
 );
 
-insert into employees values
-(1, 'Rohan Mehta', 'IT', 'Hyderabad'),
-(2, 'Sneha Iyer', 'IT', 'Bangalore'),
-(3, 'Kiran Patel', 'Finance', 'Mumbai'),
-(4, 'Ananya Das', 'HR', 'Kolkata'),
-(5, 'Rahul Sharma', 'IT', 'Delhi'),
-(6, null, 'Marketing', 'Chennai');
+insert into students values
+(1, 'Aarav Sharma', 'Hyderabad', 22),
+(2, 'Priya Reddy', 'Bangalore', 23),
+(3, 'Rahul Verma', 'Mumbai', 24),
+(4, 'Sneha Kapoor', null, 21),
+(5, 'Vikram Singh', 'Chennai', 25),
+(6, null, 'Delhi', 22);
 
-insert into projects values
-(101, 1, 'AI Chatbot', 120000, 'Active'),
-(102, 1, 'ML Prediction', 90000, 'Active'),
-(103, 2, 'Data Warehouse', 150000, 'Active'),
-(104, 3, 'Financial Dashboard', 80000, 'Completed'),
-(105, null, 'HR Survey', 30000, 'Planning'),
-(106, 8, 'Mobile App', 100000, 'Active');
+insert into enrollments values
+(101, 1, 'MySQL', 'Abdullah Khan', 5000),
+(102, 1, 'Python', 'Abdullah Khan', 7000),
+(103, 2, 'Power BI', 'Kiran', 6000),
+(104, 3, 'Azure Data Factory', 'Sneha', 8000),
+(105, null, 'Excel', 'Rohan', 3000),
+(106, 8, 'Databricks', 'Ananya', 9000);
 
-select employees.emp_name, projects.project_name, projects.project_budget
-from employees
-inner join projects
-on employees.emp_id = projects.emp_id;
+select students.student_name, enrollments.course_name
+from students
+inner join enrollments
+on students.student_id = enrollments.student_id;
 
-select employees.emp_name, projects.project_name
-from employees
-left join projects
-on employees.emp_id = projects.emp_id;
+select students.student_name, enrollments.course_name
+from students
+left join enrollments
+on students.student_id = enrollments.student_id;
 
-select employees.emp_name, projects.project_name
-from employees
-right join projects
-on employees.emp_id = projects.emp_id;
+select students.student_name, enrollments.course_name
+from students
+right join enrollments
+on students.student_id = enrollments.student_id;
 
-select employees.emp_name, projects.project_name
-from employees
-left join projects
-on employees.emp_id = projects.emp_id
+select students.student_name, enrollments.course_name
+from students
+left join enrollments
+on students.student_id = enrollments.student_id
 union all
-select employees.emp_name, projects.project_name
-from employees
-right join projects
-on employees.emp_id = projects.emp_id;
+select students.student_name, enrollments.course_name
+from students
+right join enrollments
+on students.student_id = enrollments.student_id
+where students.student_id is null;
 
-select employees.emp_name, projects.project_name
-from employees
-cross join projects;
+select students.student_name, enrollments.course_name
+from students
+cross join enrollments;
 
-select employees.emp_name, projects.project_name
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-where employees.department = 'IT';
+select students.student_name, enrollments.course_name
+from students
+join enrollments
+on students.student_id = enrollments.student_id
+where students.city = 'Hyderabad';
 
 select *
-from projects
-where project_budget > 100000;
+from enrollments
+where fee > 6000;
 
-select employees.emp_name, projects.project_name
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-where employees.city = 'Hyderabad';
+select students.student_name, count(enrollments.course_name)
+from students
+join enrollments
+on students.student_id = enrollments.student_id
+group by students.student_id, students.student_name;
 
-select employees.emp_name, count(*) as total_projects
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-group by employees.emp_name;
+select students.student_name, sum(enrollments.fee)
+from students
+join enrollments
+on students.student_id = enrollments.student_id
+group by students.student_id, students.student_name;
 
-select employees.emp_name, sum(projects.project_budget) as total_budget
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-group by employees.emp_name;
+select students.student_name, count(enrollments.course_name)
+from students
+join enrollments
+on students.student_id = enrollments.student_id
+group by students.student_id, students.student_name
+having count(enrollments.course_name) > 1;
 
-select employees.department, avg(projects.project_budget) as average_budget
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-group by employees.department;
+select enrollments.trainer, sum(enrollments.fee)
+from enrollments
+group by enrollments.trainer
+having sum(enrollments.fee) > 10000;
 
-select employees.department, count(*) as total_projects
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-group by employees.department;
+select city, count(student_id)
+from students
+group by city
+having count(student_id) > 1;
 
-select employees.department, sum(projects.project_budget) as total_dep_budget
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-group by employees.department;
-
-select city, count(*) as emp_count
-from employees
-group by city;
-
-select employees.emp_name, count(*) as more_project_count
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-group by employees.emp_name
-having count(*) > 1;
-
-select employees.department, sum(projects.project_budget) as total_budget_150k
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-group by employees.department
-having sum(projects.project_budget) > 150000;
-
-select employees.emp_name, sum(projects.project_budget) as total_budget_100k
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-group by employees.emp_name
-having sum(projects.project_budget) > 100000;
-
-select employees.emp_name, employees.department, sum(projects.project_budget) as total_budget_over_100k
-from employees
-join projects
-on employees.emp_id = projects.emp_id
-group by employees.emp_name, employees.department
-having sum(projects.project_budget) > 100000
-order by sum(projects.project_budget) desc;
+select students.student_name, students.city, sum(enrollments.fee) as total_fee_over_5k
+from students
+join enrollments
+on students.student_id = enrollments.student_id
+group by students.student_name, students.city
+having sum(enrollments.fee) > 5000
+order by sum(enrollments.fee) desc;
